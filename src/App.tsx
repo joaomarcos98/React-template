468c4699ac8ddb3c1f54fd4f300283a01d1754de
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-01-26 08:30:22 -0300

    Adding eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import ReactThumb from "@/assets/react.png"
 
 
 const App = () => {
+
     return (
         <div className="app">
-            <img src={ReactThumb} alt="Logo do React" role="img"/>
+            <img src={ReactThumb} alt="Logo do React" role="img" />
             <h1>Welcome to React</h1>
         </div>
     )
